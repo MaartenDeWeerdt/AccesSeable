d924d12ee9beb924493ea8c3bb9a1fe0c46513d7
mobapp09 <mobapp09@dt-c202-m01.local>
2017-03-14 11:03:13 +0100

    commit Maarten

---
+++ Accesseable/Restaurants.swift
@@ -0,0 +1,16 @@
+//
+//  Restaurants.swift
+//  Accesseable
+//
+//  Created by mobapp09 on 14/03/17.
+//  Copyright © 2017 mobapp09. All rights reserved.
+//
+
+import Foundation
+
+public class Restaurants{
+    
+    let restaurants[]
+    
+    
+}
